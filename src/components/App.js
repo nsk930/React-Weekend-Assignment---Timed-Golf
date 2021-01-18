436060c7b4ec3748d7c246e6afa0602a8deb26fa
nsk930 <nsk930@gmail.com>
2021-01-18 20:21:15 +0530

    nisss

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,70 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { time: 0, x: 0, y: 0 };
-  }
-  componentDidMount() {
-    
-  }
 
-  componentWillUnmount() {
-    
-  }
-
-
-
-  render() {
-    return (
- <>
-</>
+const App = () => {
+  const [renderBall, setRenderBall] = useState(false);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [ballPosition,setBallPosition] = useState({
+    left: "0px",
+    top: "0px",
+  });
+  const reset = () => {
+    setRenderBall(false);
+    updateXY(0,0);
+  };
+  const start = () => {
+    setRenderBall(true);
+  };
+  const renderChoice = () => {
+    return renderBall ? ( <div className="ball" style={{
+      position: 'absolute',
+      left: ballPosition.left,
+      top: ballPosition.top
+    }}></div>): (
+    <button onClick={start} className="start">
+      Start
+    </button>
     );
-  }
-}
+  };
 
-export default Timer;
+  const updateXY = (newX,newY) => {
+    setX(newX);
+    setY(newY);
+    setBallPosition({
+      left: newX + "px",
+      top: newY + "px"
+    });
+  }
+  useEffect(() => {
+    const keyListner = (evt) => {
+      console.log('Listen key');
+      if(renderBall) {
+      if(evt.keyCode === 37) {
+         updateXY(x-5,y);
+      } else if(evt.keyCode === 38) {
+        updateXY(x,y-5);
+      } else if(evt.keyCode === 39) {
+        updateXY(x+5,y);
+      } else if(evt.keyCode === 40) {
+        updateXY(x,y+5);
+      }
+    }
+    };
+    document.addEventListener('keydown',keyListner)
+
+    // cleanup 
+    return () => document.removeEventListener('keydown',keyListner);
+  });
+  return (
+    
+    <div className="playground">
+      <button onClick={reset} className="reset">
+        Reset
+      </button>
+      {renderChoice()}
+    </div>
+  );
+};
+
+export default App;
